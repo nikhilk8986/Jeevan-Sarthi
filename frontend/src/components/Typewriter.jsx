7eb7b2c81b858388eb72462e158cb141edf90d24
SAYAN02-DEV <sayan2003.dev@gmail.com>
2025-08-15 15:48:17 +0530

    background added

--- frontend/src/components/Typewriter.jsx
+++ frontend/src/components/Typewriter.jsx
@@ -2,16 +2,17 @@ import Typewriter from 'typewriter-effect';
 
 export default function TypewriterComponent() {
   return (
-    <div className="font-dm-serif pt-70 text-black text-center text-5xl">
-      
-      <Typewriter
-        options={{
-          strings: ['Your blood there HOPE!', 'Join Thousand Of Active Donors.', "You Don't have to be Doctor to SAVE Life."],
-          autoStart: true,
-          loop: true,
-          delay: 75,
-        }}
-      />
+    <div className="font-dm-serif pt-16 text-center text-4xl sm:text-5xl lg:text-6xl">
+      <div className="text-black drop-shadow-lg">
+        <Typewriter
+          options={{
+            strings: ['Your blood there HOPE!', 'Join Thousand Of Active Donors.', "You Don't have to be Doctor to SAVE Life."],
+            autoStart: true,
+            loop: true,
+            delay: 75,
+          }}
+        />
+      </div>
     </div>
   );
 }
